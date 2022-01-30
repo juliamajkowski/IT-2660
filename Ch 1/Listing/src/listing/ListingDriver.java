6602c9553f6c362c467b2cbd3ad67b243c64a6f0
Julia Majkowski <77689607+juliamajkowski@users.noreply.github.com>
2022-01-29 19:26:15 -0500

    Initial commit

---
+++ Ch 1/Listing/src/listing/ListingDriver.java
@@ -0,0 +1,8 @@
+package listing;
+public class ListingDriver{
+    public static void main(String[] args){
+        Listing info = new Listing();
+        info.Input();
+        System.out.println(info.toString());
+    }
+} 
